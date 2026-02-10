8c3c59cbaa242cc2a2390f0708dc136b43bf19c5
amitsingh-bit <amitsinghme80521@gmail.com>
2026-02-10 15:12:49 +0530

    sum of matrices

--- matrixSum.java
+++ matrixSum.java
@@ -4,32 +4,32 @@ public class matrixSum {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number of rows: ");
-        int rows = sc.nextInt();
+        int r = sc.nextInt();
         System.out.print("Enter the number of columns: ");
-        int cols = sc.nextInt();
-        int a[][] = new int[rows][cols];
-        int b[][] = new int[rows][cols];
+        int c = sc.nextInt();
+        int a[][] = new int[r][c];
+        int b[][] = new int[r][c];
         System.out.println("Enter the elements of the first  matrix:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 a[i][j] = sc.nextInt();   
             }
         }
         System.out.println("Enter the elements of the second matrix:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 b[i][j] = sc.nextInt();   
             }
         }
-        int sum[][] = new int[rows][cols];
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        int sum[][] = new int[r][c];
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 sum[i][j] = a[i][j] + b[i][j];
             }
         }
         System.out.println("Sum of the two matrices:");
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 System.out.print(sum[i][j] + " ");
             }
             System.out.println();
