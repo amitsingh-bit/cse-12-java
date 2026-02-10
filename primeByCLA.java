8c5ba797ea7f270b6da6c5bdf0ebe324a8c28d1e
amitsingh-bit <amitsinghme80521@gmail.com>
2026-02-10 15:42:37 +0530

    prime by command line argument

---
+++ primeByCLA.java
@@ -0,0 +1,25 @@
+public class primeByCLA {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a number as a command line argument.");
+            return;
+        }
+        int num = Integer.parseInt(args[0]);
+        boolean isPrime = true;
+        if (num <= 1) {
+            isPrime = false;
+        } else {
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+        }
+        if (isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
