ac7b5b0f611e3af6129a5f757db4a5e90e3f26da
amitsingh-bit <amitsinghme80521@gmail.com>
2026-02-10 15:46:12 +0530

    palindrome by command line argument

---
+++ palindromeByCLA.java
@@ -0,0 +1,20 @@
+public class palindromeByCLA {
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a number as a command line argument.");
+            return;
+        }
+        int num = Integer.parseInt(args[0]);
+        int original = num;
+        int reversed = 0;
+        while (num != 0) {
+            reversed = reversed * 10 + num % 10;
+            num /= 10;
+        }
+        if (original == reversed) {
+            System.out.println(original + " is a palindrome.");
+        } else {
+            System.out.println(original + " is not a palindrome.");
+        }
+    }
+}
