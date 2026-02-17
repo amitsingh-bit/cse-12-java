2e3aa7852e8cf61aa14cbb3537599936f388f615
amitsingh-bit <amitsinghme80521@gmail.com>
2026-02-17 09:44:14 +0530

    catch finally use for sum and avg

--- tester1.java
+++ tester1.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class tester1 {
     public static void main(String[] args) {
-        int []num={2,4,5,6,10};
-        int sum=0;
-        float count=5;
-        for(int a:num){
-            sum+=a;
+        try {
+            Scanner scan = new Scanner(System.in);
+            System.out.println("ENTER TOTAL NUMBER: ");
+            int count = scan.nextInt();
+            int []num={2,4,5,6,10};
+            int sum=0;
+            
+            for(int i=0;i<count;i++){
+                sum+=num[i];
+                System.out.println("sum is "+sum);
+            }
+            float avg=sum/count;
+            System.out.println("average is "+avg);
         }
-        float avg=sum/count;
-        System.out.println("average is "+avg);
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        finally{
+            System.out.println("see you later!!");
+        }
     }
 }
