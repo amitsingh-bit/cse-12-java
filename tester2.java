dc5a2782a995ee035383ece5db16ab78e70eb60e
amitsingh-bit <amitsinghme80521@gmail.com>
2026-02-18 14:29:14 +0530

    ROLL. NO WITH EXCEPTION

---
+++ tester2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class tester2 {
+    public static void main(String[] args) {
+        int marks,roll;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("ENTER ROLL NUMBER: ");
+        roll = scan.nextInt();
+        System.out.println("ENTER MARKS: ");
+        marks = scan.nextInt();
+        System.out.println("ROLL NUMBER: "+roll);
+        System.out.println("MARKS: "+marks);
+    }
+}
