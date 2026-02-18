fbd1bb216e9523b04927685a0f61e0f3c19f9bcd
amitsingh-bit <amitsinghme80521@gmail.com>
2026-02-18 14:42:02 +0530

    roll no and marks percentage check with throw

--- tester2.java
+++ tester2.java
@@ -2,12 +2,24 @@ import java.util.Scanner;
 public class tester2 {
     public static void main(String[] args) {
         int marks,roll;
+        try{
         Scanner scan = new Scanner(System.in);
         System.out.println("ENTER ROLL NUMBER: ");
         roll = scan.nextInt();
-        System.out.println("ENTER MARKS: ");
+        if(roll<0){
+            throw new Exception("ROLL NUMBER SHOULD BE LARGER THAN ZERO!!");
+        }
+        System.out.println("ENTER MARKS PERCENTAGE: ");
         marks = scan.nextInt();
+        if(marks<0||marks>100){
+            throw new Exception("MARKS PERCENTAGE SHOULD BE LESS THAN 100!!");
+        }
         System.out.println("ROLL NUMBER: "+roll);
         System.out.println("MARKS: "+marks);
+        }
+        catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+        
     }
 }
