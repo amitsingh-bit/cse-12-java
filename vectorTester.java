4d33c0e691ba99e8835918cfc8f64e00c2dcf6b8
amitsingh-bit <amitsinghme80521@gmail.com>
2026-03-30 14:51:51 +0530

    vector tester

---
+++ vectorTester.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class vectorTester {
+    public static void main(String[] args) {
+        Vector<String> vec = new Vector<>();
+        vec.add("Apple");
+        vec.add("Banana");
+        vec.add("Cherry");
+        vec.add("Orange");
+        
+        System.out.println(vec);
+    }
+}
