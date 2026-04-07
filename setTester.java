20f08685ef45dff399caca72ecea04436f8857af
amitsingh-bit <amitsinghme80521@gmail.com>
2026-04-07 14:17:15 +0530

    set tester

---
+++ setTester.java
@@ -0,0 +1,10 @@
+import java.util.*;
+void main(){
+    Set<Integer> list1 = new HashSet<>();
+    list1.add(1);
+    list1.add(2);
+    list1.add(null);
+    list1.add(-5);
+    list1.add(0);
+    System.out.println(list1);
+}
