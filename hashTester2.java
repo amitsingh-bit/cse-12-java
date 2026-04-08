48d1aa079e925815bc42316f50a4262c582aa6fc
amitsingh-bit <amitsinghme80521@gmail.com>
2026-04-08 14:34:47 +0530

    hash tester 2

---
+++ hashTester2.java
@@ -0,0 +1,22 @@
+import java.util.HashMap;
+
+void main(){
+    Map<String,Integer> map = new LinkedHashMap<>();
+    map.put("ravi",88);
+    map.put("kavi",45);
+    map.put("bavi",52);
+    System.out.println(map);
+    map.put("bavi",22);
+    System.out.println(map);
+    map.put("bavi",null);
+    System.out.println(map);
+    
+    Map<String,Integer> map2 = new HashMap<>();
+    map2.putAll(map);
+    System.out.println(map2);
+    System.out.println(map.get("ravi"));
+    System.out.println(map.getOrDefault("Ramesh", null));
+    map2.forEach((k, v) -> {
+        System.out.println(k + ":" + v);
+    });
+}
